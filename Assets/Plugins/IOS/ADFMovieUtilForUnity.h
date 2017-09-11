d8619f3f336f770ee8780612c8d1771da781b361
Ken Hakuro <ken.hakuro@gmail.com>
2017-09-11 13:35:53 +0900

    不要ファイルを削除

--- Assets/Plugins/IOS/ADFMovieUtilForUnity.h
+++
@@ -1,5 +0,0 @@
-@interface ADFMovieUtilForUnity : NSObject
-+ (const char *)convUnityParamFormat:(NSString * )stateName appID:(NSString *)appID adnetworkKey:(NSString *)adnetworkKey;
-+ (const char *)convUnityParamFormat:(NSString * )stateName appID:(NSString *)appID errCode:(NSString *)errCode;
-+ (const char *)convUnityParamFormat:(NSString * )stateName appID:(NSString *)appID;
-@end
